a39f3d54f7ef70508268c37347e4fb36ac862991
L <275166294@qq.com>
2024-05-06 15:34:19 +0800

    pom

---
+++ src/main/java/com/jingbeifang/Application.java
@@ -0,0 +1,11 @@
+package com.jingbeifang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
